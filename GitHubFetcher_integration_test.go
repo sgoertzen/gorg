ce78520493425632dcf31c7023cc44e2346c4cc5
Shawn Goertzen <sgoertzen@gmail.com>
2016-03-14 09:22:05 -0700

    Initial commit

---
+++ GitHubFetcher_integration_test.go
@@ -0,0 +1,19 @@
+// +build integration
+
+package repoclone
+
+import (
+	"github.com/stretchr/testify/assert"
+    "os"
+	"testing"
+)
+
+func TestClone(t *testing.T) {
+	err := CloneAllRepos("RepoFetch")
+    assert.Nil(t, err)
+    
+    _, err = os.Stat("fuzzy-octo-parakeet")
+    assert.False(t, os.IsNotExist(err))
+    
+    os.RemoveAll("fuzzy-octo-parakeet")
+}
