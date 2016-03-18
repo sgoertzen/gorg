56b696f4587552a47b9620b5cb5812447c74f704
Shawn Goertzen <sgoertzen@gmail.com>
2016-03-18 10:46:49 -0700

    Add missing file

---
+++ cmd/repoclone/repoclone.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/sgoertzen/repoclone"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+type config struct {
+	organization *string
+	debug        *bool
+}
+
+// Clone all the repos of an orgnaization
+func main() {
+	config := getConfiguration()
+    repoclone.SetDebug(*config.debug)
+	repoclone.CloneOrUpdateRepos(*config.organization)
+}
+
+func getConfiguration() config {
+	config := config{}
+	config.organization = kingpin.Arg("organization", "GitHub organization that should be cloned").Required().String()
+	config.debug = kingpin.Flag("debug", "Output debug information during the run.").Default("false").Short('d').Bool()
+	kingpin.Version("1.0.0")
+	kingpin.CommandLine.VersionFlag.Short('v')
+	kingpin.CommandLine.HelpFlag.Short('?')
+	kingpin.Parse()
+	return config
+}
